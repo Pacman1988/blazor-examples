d0870b3cc60c33b24a403923eb4dc5f387706ce2
Pacman313313 <office@marcowagner.at>
2023-04-09 20:35:29 +0200

    Added more examples.

---
+++ BlazorExamples/Models/PersonModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorExamples.Models;
+
+public class PersonModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
