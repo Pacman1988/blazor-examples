ad352d6bd05100d4f882dcd44a058ef4e41ad30c
Marco Wagner <marco.wagner@prismalife.com>
2023-04-09 22:55:09 +0200

    Added features

---
+++ BlazorExamples/wwwroot/js/app.js
@@ -0,0 +1,8 @@
+function exampleFunction()
+{
+    alert("Hello People");
+}
+
+function exampleFunctionWithParameters(parameter, parameter2) {
+    alert("Hello " + parameter + " and " + parameter2);
+}
